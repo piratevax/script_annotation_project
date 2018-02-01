1fa1c462e8d9bc05cf71a7d7439829a8cdd508b6
piratevax <leborgne.pirate@yahoo.fr>
2018-02-01 11:11:33 +0100

    First commit

---
+++ main.pl
@@ -0,0 +1,22 @@
+#!/usr/bin/perl 
+
+use strict;
+use warnings;
+
+use Bio::Seq;
+use Bio::SeqIO;
+use DataAnalysis;
+use Bio::Seq;
+use Bio::SeqIO;
+
+my $DEBUGG = 0;
+my ($geneSymbol, $organism, $analysis);
+
+$geneSymbol = "rad51";
+$organism = "homo sapiens";
+
+$analysis = DataAnalysis->new($geneSymbol, $organism);
+
+print $analysis->get_geneSymbol." ".$analysis->get_organism."\n" if($DEBUGG);
+
+$analysis->compute_ncbi;
