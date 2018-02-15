b713201f4398cd111d3095d9e26ee9880fc4bd7f
piratevax <leborgne.pirate@yahoo.fr>
2018-02-15 09:38:00 +0100

    Get data from ensembl

--- rest_ensembl.pl
+++ rest_ensembl.pl
@@ -3,21 +3,24 @@
 use strict;
 use warnings;
 use HTTP::Tiny;
+use JSON;
+use Data::Dumper;
 
-my ($http, $server, $response, $hash);
+my ($http, $server, $response, $ext, $hash, $species, $symbol);
 
 $http = HTTP::Tiny->new();
+$species = 'homo sapiens';
+$symbol = 'rad51';
 
 $server = 'http://rest.ensembl.org';
-$ext = '/lookup/symbol/homo_sapiens/BRCA2?expand=1';
+#$ext = "/lookup/symbol/Homo_sapiens/BRCA2?expand=1";
+$ext = '/lookup/symbol/'.join('_', split(' ', $species)).'/'.$symbol.'?expand=1';
 $response = $http->get($server.$ext, {
     headers => { 'Content-type' => 'application/json' }
 });
  
 die "Failed!\n" unless $response->{success};
  
-use JSON;
-use Data::Dumper;
 if(length $response->{content}) {
     $hash = decode_json($response->{content});
     local $Data::Dumper::Terse = 1;
