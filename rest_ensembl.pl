2a0a6e82af79910a4a0b3218e811511d5ba2e890
piratevax <leborgne.pirate@yahoo.fr>
2018-02-15 09:43:38 +0100

    Get data from EnsemblGenome

--- rest_ensembl.pl
+++ rest_ensembl.pl
@@ -11,6 +11,8 @@ my ($http, $server, $response, $ext, $hash, $species, $symbol);
 $http = HTTP::Tiny->new();
 $species = 'homo sapiens';
 $symbol = 'rad51';
+$species = 'arabidopsis thaliana';
+$symbol = 'pad4';
 
 $server = 'http://rest.ensembl.org';
 #$ext = "/lookup/symbol/Homo_sapiens/BRCA2?expand=1";
@@ -19,12 +21,31 @@ $response = $http->get($server.$ext, {
     headers => { 'Content-type' => 'application/json' }
 });
  
-die "Failed!\n" unless $response->{success};
+#die "Failed!\n" unless $response->{success};
+if ($response->{success}) { 
+    if(length $response->{content}) {
+	$hash = decode_json($response->{content});
+        local $Data::Dumper::Terse = 1;
+	local $Data::Dumper::Indent = 1;
+        print Dumper $hash;
+        print "\n";
+    }
+
+}
+else {
+    $server = 'http://rest.ensemblgenomes.org';
+    $ext = '/lookup/symbol/'.join('_', split(' ', $species)).'/'.$symbol.'?expand=1';
+    $response = $http->get($server.$ext, {
+	headers => { 'Content-type' => 'application/json' }
+    });
  
-if(length $response->{content}) {
-    $hash = decode_json($response->{content});
-    local $Data::Dumper::Terse = 1;
-    local $Data::Dumper::Indent = 1;
-    print Dumper $hash;
-    print "\n";
+    die "Failed!\n" unless $response->{success};
+ 
+    if(length $response->{content}) {
+	$hash = decode_json($response->{content});
+        local $Data::Dumper::Terse = 1;
+        local $Data::Dumper::Indent = 1;
+        print Dumper $hash;
+        print "\n";
+    }
 }
